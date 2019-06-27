1eccbe73d8e4e51499c8504a293a98dd52a16572
codeing <870439570@qq.com>
2019-06-27 18:52:41 +0800

    gorm库操作mysql
    gorm库操作mysql

--- Day02/main.go
+++ Day02/main.go
@@ -1,19 +1,12 @@
 package main
 
-
 import (
-"fmt"
-"github.com/jinzhu/gorm"
-_ "github.com/jinzhu/gorm/dialects/mysql"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"./model"
 )
 
-// 定义全局db变量
-var db *gorm.DB
-
-// 初始化db
-func init()  {
-	db, _=gorm.Open("mysql", "root:870439570@tcp(39.108.144.143:3306)/test?charset=utf8&parseTime=True&loc=Local")
-}
 
 
 type Product struct {
@@ -23,40 +16,30 @@ type Product struct {
 }
 
 func main() {
-	defer db.Close()
+	defer model.DB.Close()
 
 	// 检测指定表是否存在
-	falg:=db.HasTable("products")
+	falg:=model.DB.HasTable("products")
 	fmt.Println("指定表表名是否存在: ",falg)
 
 	// 自动迁移模式
-	db.AutoMigrate(&Product{})
+	model.DB.AutoMigrate(&Product{})
 
-	db.Create(&Product{Code:"123",Price:1000})
-
+	model.DB.Create(&Product{Code:"123",Price:1000})
 
 	// 读取
 	var product Product
-	db.First(&product,1) // 查询ID为1的数据
+	model.DB.First(&product,1) // 查询ID为1的数据
 	fmt.Println("查询ID为1的数据: ",product)
 
-	db.First(&product,"code = ?","123")  // 查询code=123
+	model.DB.First(&product,"code = ?","123")  // 查询code=123
 	fmt.Println("查询code=123的数据: ",product)
 
 
 	// 更新 上面查询出的数据
-	db.Model(&product).Update("Price",2000)
+	model.DB.Model(&product).Update("Price",2000)
 	fmt.Println("更新 上面查询出的数据: ",product)
 
-	// 删除
-	db.Delete(&product)
-
 }
 
 
-
-func check(err error)  {
-	if err !=nil{
-		panic(err)
-	}
-}
